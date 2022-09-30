3eddfdb8741fb20b90f9c691fa7e007252b11653
swa07016 <swa07016@khu.ac.kr>
2022-10-01 00:30:40 +0900

    Feature: 플레이어 입장 스크립트 처리

--- main.ts
+++ main.ts
@@ -6,3 +6,22 @@ import "zep-script";
 
 const polarBear = ScriptApp.loadSpritesheet('polar-bear.png', 240, 270);
 const polarBearTwo = ScriptApp.loadSpritesheet('polar-bear-2.png', 240, 270);
+
+// 플레이어가 해당 맵에 들어왔을 때 처리
+ScriptApp.onJoinPlayer.Add(function (p) {
+    p.spawnAt(32, 20, 2);
+    p.sprite = polarBear;
+    p.moveSpeed = 20;
+    p.tag = {
+        alive: true,
+        ready: false
+    }
+    p.title = 'READY!';
+    p.sendUpdated();
+
+    setTimeout(function () {
+        p.tag.ready = true;
+        p.title = 'SAVE ME!!';
+        p.sendUpdated();
+    }, 4000)
+});
