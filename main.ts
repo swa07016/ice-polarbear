2fb82ba96a7e58c20e78e0fff5a8c741a7928faf
swa07016 <swa07016@khu.ac.kr>
2022-10-01 00:27:26 +0900

    Feature: 북극곰 리소스 임포트

--- main.ts
+++ main.ts
@@ -4,10 +4,5 @@
 
 import "zep-script";
 
-ScriptApp.showCenterLabel("Hello World");
-
-
-// 플레이어가 스페이스에 입장 했을 때 이벤트
-ScriptApp.onJoinPlayer.Add(function(player) {
-    player.showCustomLabel(`${player.name} 학우님 입장! Let's Party!!🎉`);
-});
+const polarBear = ScriptApp.loadSpritesheet('polar-bear.png', 240, 270);
+const polarBearTwo = ScriptApp.loadSpritesheet('polar-bear-2.png', 240, 270);
